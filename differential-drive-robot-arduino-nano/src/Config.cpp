98d399b57cf519b40d13b8264639559ffcf86138
andrerusso <andrerusso02@gmail.com>
2022-07-04 16:24:44 +0200

    fonctionne avec teleop keyboard

--- differential-drive-robot-arduino-nano/src/Config.cpp
+++ differential-drive-robot-arduino-nano/src/Config.cpp
@@ -2,15 +2,15 @@
 
 namespace Pins
 {
-    int enableL = 10;
-    int dir1L = 11;
-    int dir2L = 12;
-    int encoderL = 2;
+    int enableR = 10;
+    int dir1R = 11;
+    int dir2R = 12;
+    int encoderR = 2;
 
-    int enableR = 9;
-    int dir1R = 8;
-    int dir2R = 7;
-    int encoderR = 3;
+    int enableL = 9;
+    int dir1L = 8;
+    int dir2L = 7;
+    int encoderL = 3;
 }
 
 namespace RobotModel
