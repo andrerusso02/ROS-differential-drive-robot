22989a89fc7788032e6c5c53fa39cde750a01665
andrerusso <andrerusso02@gmail.com>
2022-07-27 11:07:05 +0200

    TFmini-S fonctionne sans librairie

--- lidar-arduino/src/main.cpp
+++ lidar-arduino/src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#include <SoftwareSerial.h>   //header file of software serial port
 
+SoftwareSerial Serial1(7, 8); //define software serial port name as Serial1 and define pin2 as RX & pin3 as TX
+ 
+int dist;                     //actual distance measurements of LiDAR
+int strength;                 //signal strength of LiDAR
+int check;                    //save check value
+int i;
+int uart[9];                   //save data measured by LiDAR
+const int HEADER = 0x59;      //frame header of data package*
+
+long last_time = 0;            //last time of data package
+ 
+ 
+void setup()
+{
+  Serial.begin(115200);         //set bit rate of serial port connecting Arduino with computer
+  Serial1.begin(9600);      //set bit rate of serial port connecting LiDAR with Arduino
+}
+ 
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial1.available())                //check if serial port has data input
+  {
+    if (Serial1.read() == HEADER)        //assess data package frame header 0x59
+    {
+      uart[0] = HEADER;
+      if (Serial1.read() == HEADER)      //assess data package frame header 0x59
+      {
+        double freq = 1000. / (double)(millis () - last_time); //calculate time between two data package
+        uart[1] = HEADER;
+        for (i = 2; i < 9; i++)         //save data in array
+        {
+          uart[i] = Serial1.read();
+        }
+        check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
+        if (uart[8] == (check & 0xff))        //verify the received data as per protocol
+        {
+          dist = uart[2] + uart[3] * 256;     //calculate distance value
+          strength = uart[4] + uart[5] * 256; //calculate signal strength value
+          Serial.print("dist = ");
+          Serial.print(dist);                 //output measure distance value of LiDAR
+          //Serial.print('\t');
+          //Serial.print("strength = ");
+          //Serial.print(strength);             //output signal strength value
+          Serial.print('\t');
+          Serial.print("freq = ");
+          Serial.println(freq);               //output frequency value
+          last_time = millis();
+        }
+        else
+        {
+          Serial.println("data error");
+        }
+      }
+    }
+  }
 }
