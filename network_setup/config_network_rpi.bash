3917a029cf8a19d199689da404464c801c24b4a0
andrerusso02 <andrerusso02@gmail.com>
2022-08-25 16:36:48 +0200

    suppression dossier network_config

--- network_setup/config_network_rpi.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export ROS_MASTER_URL=http://localhost:11311
-export ROS_HOSTNAME=ddbot
-export ROS_IP=ddbot
-
--- network_setup/config_network_workstation.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export ROS_MASTER_URL=http://ddrobot:11311
-export ROS_HOSTNAME=$(hostname -I)
-export ROS_IP=$(hostname -I)
