bc7078654496ab83d3f075ded09dea811c468b13
andrerusso02 <andrerusso02@gmail.com>
2022-08-25 18:27:58 +0200

    ddbot_test renommé ddbot_run + script find_usb_devices, non testé

---
+++ ws-dev-linux/src/ddbot_run/scripts/find_usb_devices.py
@@ -0,0 +1,61 @@
+import serial
+import serial.tools.list_ports
+import time
+
+# This script finds the ports of the 3 USB peripherials of the robot.
+# If all 3 peripherials are found they are displayed as follow:
+#   Arduino_wheels /dev/tty...
+#   Arduino_lidar /dev/tty...
+#   Lidar_sensor /dev/tty...
+# If at least one of these chips aren't found, then the message "Port(s) not found" is displayed.
+
+
+WHOAMI = 0xFF # used by lidar's arduino but not wheels motors arduino
+ID = 0xFE # lidar's arduino answer to WHOAMI
+
+if __name__ == '__main__':
+
+    arduino_wheels_found = False
+    arduino_lidar_found = False
+    lidar_sensor_found = False
+
+    error = "Port(s) not found"
+
+    response = ""
+
+    peripherials = serial.tools.list_ports.comports(include_links=False)
+    for peripherial in peripherials:
+        if peripherial.description == "USB2.0-Serial":
+            port = "/dev/" + peripherial.name
+            # connect to see if arduino responds to WHOAMI
+            ser = serial.Serial(port, 115200, timeout=1.0)
+            time.sleep(2) # wait for Arduino to reboot
+            ser.write(WHOAMI.to_bytes(1, 'little'))
+            res = ser.read(1)
+            ser.close()
+            if res == ID.to_bytes(1, 'little'): # motor Arduino found !
+                if not arduino_lidar_found:
+                    arduino_lidar_found = True
+                    response += "Arduino_lidar " + port + "\n"
+                else :
+                    response = error
+                    break
+            else:
+                if not arduino_wheels_found:
+                    arduino_wheels_found = True
+                    response += "Arduino_wheels " + port + "\n"
+                else :
+                    response = error
+                    break
+        elif peripherial.description == "USB Serial":
+            if not lidar_sensor_found:
+                lidar_sensor_found = True
+                response += "Lidar_sensor " + port + "\n"
+            else :
+                response = error
+                break
+    
+    if response.count('\n'):
+        print(response, end=None)
+    else:
+        print(error)
