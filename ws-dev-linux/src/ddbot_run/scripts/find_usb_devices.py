f120f2cf68849509d9c1fcd9dd5bdb811368a459
ddbot <andrerusso02@gmail.com>
2022-08-28 10:20:32 +0000

    modification du script find_usb_devices.py pour qu'il crée un script sh exportant des variables d'environnement

--- ws-dev-linux/src/ddbot_run/scripts/find_usb_devices.py
+++ ws-dev-linux/src/ddbot_run/scripts/find_usb_devices.py
@@ -1,17 +1,19 @@
 import serial
 import serial.tools.list_ports
 import time
+import os
 
 # This script finds the ports of the 3 USB peripherials of the robot.
-# If all 3 peripherials are found they are displayed as follow:
-#   Arduino_wheels /dev/tty...
-#   Arduino_lidar /dev/tty...
-#   Lidar_sensor /dev/tty...
-# If at least one of these chips aren't found, then the message "Port(s) not found" is displayed.
-
+# It writes a scripts setting them as environnment variables and executes it.
 
 WHOAMI = 0xFF # used by lidar's arduino but not wheels motors arduino
 ID = 0xFE # lidar's arduino answer to WHOAMI
+
+ENV_ARDUINO_WHEELS = "ROS_PORT_ARDUINO_LIDAR"
+ENV_ARDUINO_LIDAR = "ROS_PORT_ARDUINO_WHEELS"
+ENV_LIDAR_SENSOR = "ROS_PORT_LIDAR_SENSOR"
+
+SCRIPT_PATH = os.path.realpath(os.path.dirname(__file__)) + "/set_usb_ports.bash"
 
 if __name__ == '__main__':
 
@@ -21,7 +23,9 @@ if __name__ == '__main__':
 
     error = "Port(s) not found"
 
-    response = ""
+    text_to_write = "#!/bin/sh\n"
+
+    print("Searching for USB ports...")
 
     peripherials = serial.tools.list_ports.comports(include_links=False)
     for peripherial in peripherials:
@@ -36,26 +40,31 @@ if __name__ == '__main__':
             if res == ID.to_bytes(1, 'little'): # motor Arduino found !
                 if not arduino_lidar_found:
                     arduino_lidar_found = True
-                    response += "Arduino_lidar " + port + "\n"
+                    text_to_write += "export " + ENV_ARDUINO_WHEELS + "=\"" + port + "\"\n"
+                    print(ENV_ARDUINO_WHEELS + "=" + port)
                 else :
-                    response = error
-                    break
+                    print("Error : Another peripherial at " + port + " matches description for " + ENV_ARDUINO_WHEELS)
             else:
                 if not arduino_wheels_found:
                     arduino_wheels_found = True
-                    response += "Arduino_wheels " + port + "\n"
+                    text_to_write += "export " + ENV_ARDUINO_LIDAR + "=\"" + port + "\"\n"
+                    print(ENV_ARDUINO_LIDAR + "=" + port)
                 else :
-                    response = error
-                    break
+                    print("Error : Another peripherial at " + port + " matches description for " + ENV_ARDUINO_LIDAR)
         elif peripherial.description == "USB Serial":
             if not lidar_sensor_found:
                 lidar_sensor_found = True
-                response += "Lidar_sensor " + port + "\n"
+                text_to_write += "export " + ENV_LIDAR_SENSOR + "=\"" + port + "\"\n"
+                print(ENV_LIDAR_SENSOR + "=" + port)
             else :
-                response = error
-                break
+                print("Error : Another peripherial at " + port + " matches description for " + ENV_LIDAR_SENSOR)
     
-    if arduino_wheels_found and arduino_lidar_found and lidar_sensor_found:
-        print(response[:-1], end=None) # don't print last \n
+    if not arduino_wheels_found and not arduino_lidar_found and not lidar_sensor_found:
+        print("No peripherials were found")
     else:
-        print(error)
+        print("Writing script setting these environnment variables at " + SCRIPT_PATH)
+        with open(SCRIPT_PATH, "w") as f:
+            f.write(text_to_write)
+            os.system("chmod +x " + SCRIPT_PATH)
+        print("Please run :\n\n\t source " + SCRIPT_PATH + "\n")
+
