d1dc0bfb54b33a534bf8e3b92da9be640581435e
ddbot <andrerusso02@gmail.com>
2022-08-29 14:32:47 +0000

    node de ros_lidar prend des parametres ros pour sa config

--- ws-dev-linux/src/ros_lidar/src/lidar_laserscan_publisher.py
+++ ws-dev-linux/src/ros_lidar/src/lidar_laserscan_publisher.py
@@ -8,12 +8,12 @@ import serial
 from rosgraph import is_master_online
 
 # scan : distances and intensities for a complete revolution
-def build_laserscan_msg(scan, duration):
+def build_laserscan_msg(scan, duration, frame_id):
     t = time.time()
     scan.reverse() # LaserScan wants measurements to be counter-clockwise
     msg = LaserScan()
     msg.header.stamp = rospy.Time.now()
-    msg.header.frame_id = "lidar" #todo vérifier que c'est bien ça
+    msg.header.frame_id = frame_id #todo vérifier que c'est bien ça
     msg.angle_min = 0.0
     msg.angle_max = 3.14159265359*2
     msg.angle_increment = 3.14159265359*2/len(scan)
@@ -31,21 +31,30 @@ if __name__ == '__main__':
     if not is_master_online():
         raise Exception("ROS master not found")
 
-    # get speed from command line (rad/s)
-    if len(rospy.myargv()) != 3:
-        speed = 3.14159*3 # default
-        zero_pos = 1.68
-    else:
-        speed = float(rospy.myargv()[1])
-        zero_pos = float(rospy.myargv()[2])
+    namespace = ""
+    if len(rospy.myargv()) != 1:
+        if rospy.myargv()[1][0]!="/":
+            namespace = "/"
+        namespace += rospy.myargv()[1]
 
     rospy.init_node('lidar_laserscan_publisher', anonymous=True)
 
-    # get ports from parameter server
-    port_lidar_sensor = rospy.get_param('~port_lidar_sensor', None)
-    port_motor = rospy.get_param('~port_motor', None)
+    rospy.loginfo("LidarLaserscanPublisher node started with namespace %s", namespace)
 
-    rospy.loginfo("Started lidar_laserscan_publisher with parameters : speed=" + str(speed) + " zero_pos=" + str(zero_pos) + " port_lidar_sensor=" + str(port_lidar_sensor) + " port_motor=" + str(port_motor))
+    # get params from parameter server
+    try:
+        port_lidar_sensor = rospy.get_param(namespace + "/port_lidar_sensor") # rad/s
+        port_motor = rospy.get_param(namespace + "/port_motor") # rad : offset from hall sensor
+        speed = rospy.get_param(namespace + "/speed")
+        zero_pos = rospy.get_param(namespace + "/zero_position")
+        lidar_frame_id = rospy.get_param(namespace + "/lidar_frame_id")
+    except Exception as e:
+        rospy.logerr("Parameters not found, aborting..." + str(e))
+        time.sleep(1.0) # wait for log to be published
+        rospy.signal_shutdown("Parameters not found")
+        exit()
+
+    rospy.loginfo("Started lidar_laserscan_publisher with parameters : speed=" + str(speed) + " zero_pos=" + str(zero_pos) + " port_lidar_sensor=" + str(port_lidar_sensor) + " port_motor=" + str(port_motor) + " lidar_frame_id=" + lidar_frame_id)
 
     lidar = Lidar(port_lidar_sensor, port_motor)
     lidar.start(speed, zero_pos)
@@ -63,7 +72,7 @@ if __name__ == '__main__':
             if(rospy.is_shutdown()):
                 break
         t_now = rospy.Time.now()
-        msg = build_laserscan_msg(distances, t_now - t_last)
+        msg = build_laserscan_msg(distances, t_now - t_last, lidar_frame_id)
         pub_laserscan.publish(msg)
         t_last = t_now
         if(rospy.is_shutdown()):
